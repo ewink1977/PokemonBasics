71646b0cb40180bf48748c36ad303a4aba600ab8
Erin Winking <ewink@yahoo.com>
2021-01-31 19:35:28 -0800

    User, Pokemon models made. Migrations done. LOGIN/REG page laid out.

--- PokeUsersApp/models.py
+++ PokeUsersApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    poke_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class User(models.Model):
+    username = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 100)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    pokemon_captured = models.ManyToManyField(
+        Pokemon,
+        related_name = 'captured_by'
+    )
--- PokeUsersApp/views.py
+++ PokeUsersApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import User, Pokemon
 
 def home(request):
     return render(request, 'userHTML/home.html')
